fc266b94eca741acff78bda401dafaabbad62f6b
= <corepper03@gmail.com>
2024-07-22 14:30:12 +0700

    feat(vite): Config build other pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 const { defineConfig } = require("vite");
-
+const { resolve } = require("path");
 module.exports = defineConfig({
   root: "./",
   base: "./",
   build: {
     outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        discover: resolve(__dirname, "discover.html"),
+        blog: resolve(__dirname, "blog.html"),
+        fundedGrantDetail: resolve(__dirname, "funded-grant-detail.html"),
+        fundedGrant: resolve(__dirname, "funded-grant.html"),
+        grantee: resolve(__dirname, "grantee.html"),
+      },
+    },
   },
 });
